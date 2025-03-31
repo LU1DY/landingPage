6a9f7415f50d1042956af09adadcabcd631f9a0e
Luidy-Michael <luidymichael8@gmail.com>
2025-03-31 15:52:14 -0300

    sessao hero

--- app/js/script.js
+++ app/js/script.js
@@ -4,3 +4,11 @@ function toggleMenu() {
   menu.classList.toggle("visible");
 }
 btnCheckbox.addEventListener("click", toggleMenu);
+
+window.addEventListener("offline", () => {
+  alert("Você esta offline");
+});
+
+window.addEventListener("online", () => {
+  alert("Você esta online");
+});
